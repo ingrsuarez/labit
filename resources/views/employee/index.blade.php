98509d716afda63b0be48a29e2e2f046031dbff2
Rodrigo <ing.rsuarez@gmail.com>
2025-08-17 22:50:05 -0300

    elimino la necesidad de que exista un job

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -78,9 +78,9 @@
 
         <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
         
-
+            @livewire('organization-chart', ['employees' => $employees, 'job' => $job ?? null])
             <!-- component -->
-            @livewire('organization-chart',['employees'=>$employees, 'job'=>$job])
+            {{-- @livewire('organization-chart',['employees'=>$employees, 'job'=>$job]) --}}
 
 
         </div>
