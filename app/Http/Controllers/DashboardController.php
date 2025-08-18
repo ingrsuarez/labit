8dbddc32a874e1943a6c42bc34ad2714c1c4baf0
Rodrigo <ing.rsuarez@gmail.com>
2025-08-17 22:44:38 -0300

    Agrego dashboardcontroller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+        return view('dashboard');
+        return $user;
+    }
+}
