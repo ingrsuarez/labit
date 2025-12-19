33b6c40c92b3f9d5b39021c518fb0b12ee6d79e0
Rodrigo <ing.rsuarez@gmail.com>
2025-12-19 09:43:20 -0300

    soluciono problema de vista

--- app/Models/Holiday.php
+++ app/Models/Holiday.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Holiday extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'date',
+        'type',
+        'is_optional',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'is_optional' => 'boolean',
+    ];
+
+    /**
+     * Scope para feriados obligatorios
+     */
+    public function scopeObligatory($query)
+    {
+        return $query->where('is_optional', false);
+    }
+
+    /**
+     * Scope para un año específico
+     */
+    public function scopeForYear($query, int $year)
+    {
+        return $query->whereYear('date', $year);
+    }
 }
