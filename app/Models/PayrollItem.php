05dd0021c4ecff8e9b41227322fcb0b1e58d0201
Rodrigo <ing.rsuarez@gmail.com>
2025-12-23 19:55:55 -0300

    agrego payroll item

--- app/Models/PayrollItem.php
+++ app/Models/PayrollItem.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class PayrollItem extends Model
 {
-    //
+    protected $fillable = [
+        'payroll_id',
+        'type',           // 'haber' o 'deduccion'
+        'name',
+        'percentage',
+        'amount',
+        'is_remunerative',
+        'order',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'is_remunerative' => 'boolean',
+    ];
+
+    /**
+     * Relación con el recibo
+     */
+    public function payroll()
+    {
+        return $this->belongsTo(Payroll::class);
+    }
 }
